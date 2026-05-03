5db8381407bb864083a8a4a429bd754b0f7aa896
d9ff1ck <dimadadonov01@gmail.com>
2026-05-03 06:50:55 +0200

    refactor(react-dom): simplify polymorphic prop types

--- packages/react-dom/src/types/PolymorphicComponentWithRef.ts
+++ packages/react-dom/src/types/PolymorphicComponentWithRef.ts
@@ -1,13 +1,11 @@
 import type { ComponentPropsWithRef, ElementType, Ref } from "react";
 
-import type { ComponentWithChildren } from "./ComponentWithChildren";
-import type { ComponentWithStyles } from "./ComponentWithStyles";
-import type { ElementOf } from "./ElementOf";
+import type { ElementOf, MergeProps } from "@/types";
 
-export type PolymorphicComponentWithRef<T extends ElementType, P> = {
-    as?: T;
-    ref?: Ref<ElementOf<T>>;
-} & P &
-    ComponentWithChildren &
-    ComponentWithStyles &
-    Omit<ComponentPropsWithRef<T>, keyof ComponentWithChildren | keyof ComponentWithStyles | keyof P | "as" | "ref">;
+export type PolymorphicComponentWithRef<T extends ElementType, P> = MergeProps<
+    {
+        as?: T;
+        ref?: Ref<ElementOf<T>>;
+    } & P,
+    ComponentPropsWithRef<T>
+>;
--- packages/react-dom/src/types/PolymorphicComponentWithoutRef.ts
+++ packages/react-dom/src/types/PolymorphicComponentWithoutRef.ts
@@ -1,14 +1,10 @@
 import type { ComponentPropsWithoutRef, ElementType } from "react";
 
-import type { ComponentWithChildren } from "./ComponentWithChildren";
-import type { ComponentWithStyles } from "./ComponentWithStyles";
-import type { MergeProps } from "./MergeProps";
+import type { MergeProps } from "@/types";
 
 export type PolymorphicComponentWithoutRef<T extends ElementType, P = object> = MergeProps<
     {
         as?: T;
-    } & P &
-        ComponentWithStyles &
-        ComponentWithChildren,
+    } & P,
     ComponentPropsWithoutRef<T>
 >;
